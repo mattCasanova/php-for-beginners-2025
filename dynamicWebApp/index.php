4ccb195f2820853223b130c89cc16981c1efd7c6
Matt Casanova <lazersquad@gmail.com>
2025-08-31 13:42:43 -0700

    End of Ep PDO First Steps

--- dynamicWebApp/index.php
+++ dynamicWebApp/index.php
@@ -1,4 +1,19 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+// connect to our MySQL database
+
+$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4';
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . htmlspecialchars($post['title']). "</li>";
+}
