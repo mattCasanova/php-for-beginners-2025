ea29c4fce464efa2ea72696963816c5fc1f6f369
Matt Casanova <lazersquad@gmail.com>
2025-09-01 12:42:02 -0700

    End Ep Escape input

--- notesMiniProject/controllers/note-create.php
+++ notesMiniProject/controllers/note-create.php
@@ -1,9 +1,16 @@
 <?php
 
+$config = require 'config.php';
+$db = new Database($config['database']);
+
+
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $body = $_POST['body'] ?? '';
+    $db->query('insert into notes (body, user_id) values (:body, 1)', [
+        'body' => $body
+    ]);
 
 }
 
