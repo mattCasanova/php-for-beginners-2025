2f7f7f5c30e13a6ce93e62755256c1bcaee256c8
Matt Casanova <lazersquad@gmail.com>
2025-09-01 04:11:54 -0700

    Starting Notes MiniProject

---
+++ notesMiniProject/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+// Connect to a database using PDO (PHP Data Objects) and execture a query
+class Database
+{
+    public $connection;
+
+    public function __construct(array $config, string $username = 'root', string $password = '')
+    {
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query, $params = [])
+    {
+        $statement = $this->connection->prepare($query);
+        $statement->execute($params);
+
+        return $statement;
+    }
+}
