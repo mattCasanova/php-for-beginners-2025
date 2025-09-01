d840d41fd97cfe9f72d7b8ec8cfa4e280d777391
Matt Casanova <lazersquad@gmail.com>
2025-09-01 14:48:29 -0700

    End of Ep Form Validation

--- notesMiniProject/controllers/note-create.php
+++ notesMiniProject/controllers/note-create.php
@@ -7,11 +7,22 @@ $db = new Database($config['database']);
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errors = [];
     $body = $_POST['body'] ?? '';
-    $db->query('insert into notes (body, user_id) values (:body, 1)', [
-        'body' => $body
-    ]);
+    $length = strlen($body);
 
+
+    if ($length === 0) {
+        $errors['body'] = 'A body is required';
+    } elseif ($length > 1000) {
+        $errors['body'] = 'Body cannot be longer than 1000 characters';
+    }
+
+    if (empty($errors)) {
+        $db->query('insert into notes (body, user_id) values (:body, 1)', [
+            'body' => $body
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
--- notesMiniProject/views/note-create.view.php
+++ notesMiniProject/views/note-create.view.php
@@ -10,7 +10,19 @@
           <div class="col-span-full">
             <label for="body" class="block text-sm/6 font-medium text-gray-900">Note</label>
             <div class="mt-2">
-              <textarea id="body" name="body" rows="3" placeholder="Here's an idea for a note..." class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+              <textarea
+              id="body"
+              name="body"
+              rows="3"
+              placeholder="Here's an idea for a note..."
+              required
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              ><?= $_POST['body'] ?? '' ?></textarea>
+
+              <?php if (! empty($errors['body'])) : ?>
+                <p class="mt-2 text-sm text-red-500"><?= htmlspecialchars($errors['body']) ?></p>
+              <?php endif; ?>
+
             </div>
           </div>
         </div>
