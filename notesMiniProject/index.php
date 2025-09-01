e7e79c8ce092cd8e628080f40f370fc72f45a19d
Matt Casanova <lazersquad@gmail.com>
2025-09-01 04:17:43 -0700

    Updated index.php for new notes project so it works

--- notesMiniProject/index.php
+++ notesMiniProject/index.php
@@ -2,14 +2,7 @@
 
 require 'functions.php';
 require 'Database.php';
-//require 'router.php';
+require 'router.php';
 
 $config = require 'config.php';
 $db = new Database($config['database']);
-
-$id = $_GET['id'];
-$query = "select * from posts where id = :id";
-
-$posts = $db->query($query, [':id' => $id])->fetch();
-
-dd($posts);
