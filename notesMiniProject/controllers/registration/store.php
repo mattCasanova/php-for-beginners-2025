c35bb8df57b32e5449d871c26fb7a7cc7606e83a
Matt Casanova <lazersquad@gmail.com>
2025-09-04 21:29:17 -0700

    End Ep Register new user

--- notesMiniProject/controllers/registration/store.php
+++ notesMiniProject/controllers/registration/store.php
@@ -41,7 +41,6 @@ $db->query('insert into users (email, password) values (:email, :password)', [
 ]);
 
 // mark the user as logged in
-$_SESSION['logged_in'] = true;
 $_SESSION['user'] = [
     'email' => $email,
 ];
